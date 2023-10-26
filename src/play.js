272a62c83f306943751e75d44c6e82ff7eabada6
lhdmir <lhdmir@naver.com>
2023-10-26 11:07:35 +0900

    배경 전역변수 선언 삭제

--- src/play.js
+++ src/play.js
@@ -3,7 +3,6 @@ import Phaser from "phaser";
 export default class PlayScene extends Phaser.Scene {
   constructor() {
     super({ key: "PlayScene" });
-    this.bg = null;
   }
 
   preload() {
--- src/title.js
+++ src/title.js
@@ -5,7 +5,6 @@ import Phaser from "phaser";
 export default class TitleScene extends Phaser.Scene {
   constructor() {
     super({ key: "TitleScene" });
-    this.bg = null;
   }
 
   preload() {
