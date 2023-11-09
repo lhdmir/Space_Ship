629bf971f5e14831e611d159c954c1c2f476b2da
lhdmir <lhdmir@naver.com>
2023-11-09 14:08:47 +0900

    플레이어 체력 수정

--- src/Character/Player.js
+++ src/Character/Player.js
@@ -15,7 +15,7 @@ export default class Player extends Phaser.Physics.Arcade.Sprite {
     super(scene, 400, 600, "Player");
 
     // this.PLAYER_HP = 100;
-    this.PLAYER_HP = 100;
+    this.PLAYER_HP = 30;
 
     // 이동 가능한지 체크하는 플래그 추가
     this.isMoveable = false;
